e05b529b6d6fb0b90bd869f3dd38c24c563ae549
Alish Bista <bistace321@gmail.com>
2024-03-09 16:23:32 +0545

    Feat: Written Department Model

---
+++ src/models/department.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const departmentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Department name is required'],
+    unique: true
+  },
+  code: {
+    type: String,
+    required: [true, 'Department code is required'],
+    unique: true
+  },
+  head: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Professor', // Assuming there's a Professor model
+    default: null
+  },
+  courses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course', // Assuming there's a Course model,
+    default: null
+  }]
+});
+
+const Department = mongoose.model('Department', departmentSchema);
+
+module.exports = Department;
